4237415655d731c3e0186b803cc904f2257bb951
Eric Wang <37554696+ericwang401@users.noreply.github.com>
2022-09-01 23:39:53 -0500

    Uncomment check for if image exists in PVE

--- main.go
+++ main.go
@@ -163,12 +163,12 @@ func getImages(wg *sync.WaitGroup) []Template {
 		wg.Add(1)
 
 		go func(image Template) {
-			/* if err := exec.Command("bash", "-c", fmt.Sprintf("qm status %d", image.VMID)).Run(); err != nil {
+			if err := exec.Command("bash", "-c", fmt.Sprintf("qm status %d", image.VMID)).Run(); err != nil {
 				fmt.Printf("Image '%s' already exists!\n", image.Name)
 
 				wg.Done()
 				return
-			} */
+			}
 
 			if _, err := os.Stat(fileName); err != nil {
 				DownloadFile(image.Link, "./", fileName, image.Name, p)
